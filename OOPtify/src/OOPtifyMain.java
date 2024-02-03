4121e95953c160bb1c7e3da07c36e357d66e7391
John Glay Bunao <johnglayc.bunao@gmail.com>
2024-02-03 17:17:50 +0800

    Set up of OOPtifyMain

--- OOPtify/src/OOPtifyMain.java
+++ OOPtify/src/OOPtifyMain.java
@@ -1,6 +1,34 @@
-public class OOPtifyMain {
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class OOPtifyMain extends JFrame implements ActionListener {
+
+    public OOPtifyMain() {
+        setLayout(null);
+
+        // JUSTIN PART
+
+        // JOHN GLAY PART
+
+        getContentPane().setBackground(Color.decode("#212121"));
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Test");
-        System.out.println("Test 2");
+        SwingUtilities.invokeLater(() -> {
+            OOPtifyMain frame = new OOPtifyMain();
+            frame.setSize(900, 800); // Set an initial size
+            frame.setLocationRelativeTo(null); // Center the frame on the screen
+            frame.setVisible(true);
+        });
+    }
+
+    public void itemStateChanged(ActionEvent e) {
+
+    }
+
+    public void actionPerformed(ActionEvent e) {
+
     }
 }
